ba6f74f5861bc90b9d13476a20239d446cb27d76
MominAli <momin.ali7514@gmail.com>
2025-08-25 13:22:11 +0530

    day 01 updated

--- Angular-Scenario-Based-Interview-Questions-2025/src/app/scenarios/day01/day01.component.ts
+++ Angular-Scenario-Based-Interview-Questions-2025/src/app/scenarios/day01/day01.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common'; // ✅ Required for *ngFor
 
 @Component({
   selector: 'app-day01',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './day01.component.html',
+  styles:`.day-content {
+  animation: fadeIn 0.6s ease-in;
+}
+
+@keyframes fadeIn {
+  from { opacity: 0; transform: translateY(10px); }
+  to { opacity: 1; transform: translateY(0); }
+}
+
+.blockquote {
+  font-style: italic;
+  border-left: 4px solid #ccc;
+  padding-left: 1rem;
+}
+`
 })
-export class Day01Component {}
+export class Day01Component {
+   readonly title = 'Day 01: A component is updating slowly when data changes — how would you diagnose and fix it?';
+}
